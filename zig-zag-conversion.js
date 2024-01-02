a2d8426b34c5838ccc27f0dd1f4eea926e8f8a83
HudsonAdjetey <adjetey545@gmail.com>
2024-01-02 21:34:58 +0000

    convert

---
+++ zig-zag-conversion.js
@@ -0,0 +1,21 @@
+const zigZagConversion = (s, numRows) => {
+    // check if the "s" is not going to change
+    if(numRows === 1 || s.length <= numRows){
+        return s;
+        }
+        // create rows and fill them with " " based on the rows given
+        const rows = new Array(numRows).fill("")
+        let index = 0
+        let step = 1
+
+        for(const char of s) {
+            rows[index] += char
+            if(index == 0 ){
+                step = 1
+            }else if(index == numRows - 1) {
+                step = -1
+            }
+            index += step
+        }
+        return rows.join('')
+}
